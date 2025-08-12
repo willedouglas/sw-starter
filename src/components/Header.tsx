86db52ee04a804ae04ba54a6b304b8f7b4634035
Douglas Wille <willedouglas10@gmail.com>
2025-08-12 15:08:21 -0300

    chore: adding border to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ interface HeaderProps {
 
 export default function Header({ title }: HeaderProps) {
   return (
-    <header className="bg-white h-[25px] flex items-center justify-center shadow-[0_1px_0_0_var(--color-gray)]">
+    <header className="bg-white h-[25px] flex items-center justify-center shadow-[0_1px_0_0_var(--color-gray)] border-[0.5px] border-[var(--color-warm-gray)]">
       <h1 className="text-green-teal text-sm font-bold">{title}</h1>
     </header>
   );

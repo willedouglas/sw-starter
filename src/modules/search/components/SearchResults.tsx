2ad1ce263275a9956f312e36000d9329a61670d0
Douglas Wille <willedouglas10@gmail.com>
2025-08-12 09:49:59 -0300

    feat: adding search results component

---
+++ src/modules/search/components/SearchResults.tsx
@@ -0,0 +1,41 @@
+import Card from "@/components/Card";
+import Divider from "@/components/Divider";
+import SearchEmptyState from "@/modules/search/components/SearchEmptyState";
+
+interface SearchResultItem {
+  name: string;
+  url: string;
+}
+
+interface SearchResultsProps {
+  items?: SearchResultItem[];
+  isLoading?: boolean;
+}
+
+export default function SearchResults({
+  items = [],
+  isLoading = false,
+}: SearchResultsProps) {
+  return (
+    <Card className="flex flex-col min-h-[291px] min-w-[291px]">
+      <h2 className="text-md font-bold text-black">Results</h2>
+
+      <Divider />
+
+      <div className="flex-1 flex items-center justify-center">
+        {items.length === 0 && !isLoading && <SearchEmptyState />}
+        {isLoading && <span>Searching...</span>}
+      </div>
+
+      {items.length > 0 && (
+        <ul className="w-full divide-y divide-gray-200 text-gray-700">
+          {items.map((item, idx) => (
+            <li key={idx} className="py-2">
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </Card>
+  );
+}

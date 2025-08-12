a6e2da9a26125a8d73af2c39c9f442ecfa07fcf9
Douglas Wille <willedouglas10@gmail.com>
2025-08-12 09:48:42 -0300

    feat: adding base divider component

---
+++ src/components/Divider.tsx
@@ -0,0 +1,7 @@
+import { JSX } from "react";
+
+export default function Divider(): JSX.Element {
+  return (
+    <div className="h-[0.5px] my-[5px] bg-[var(--color-pinkish-grey)] w-full" />
+  );
+}
